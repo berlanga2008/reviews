834379146880161082fabac6ecd0fc35437f3927
Miguel <berlanga2008@gmail.com>
2020-06-21 12:12:12 +0200

    Update review.php

--- review.php
+++ review.php
@@ -15,8 +15,6 @@
  * @author  WooThemes
  * @package WooCommerce/Templates
  * @version 2.5.0
- *
- * /wp-content/themes/adventure-tours/woocommerce/single-product/review.php
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -30,55 +28,44 @@ $verified = wc_review_is_from_verified_owner( $comment->comment_ID );
 
 
 <div itemprop="itemReviewed" itemscope itemtype="http://schema.org/Place">
-<span itemprop="name">Tour</span>
-<li itemprop="review" itemscope itemtype="http://schema.org/Review" class="tour-reviews__item margin-left margin-right padding-top padding-bottom" id="li-comment-<?php comment_ID() ?>">
+<span itemprop="name" style="display:none">Tour</span>
+<div itemprop="review" itemscope itemtype="http://schema.org/Review" <?php comment_class(); ?> id="li-comment-<?php comment_ID() ?>">
    
 
-	<div id="comment-<?php comment_ID(); ?>" class="comment_container">
-		<div class="tour-reviews__item__container">
-		<div class="tour-reviews__item__info">
-
+	<div id="comment-<?php comment_ID(); ?>"  class="tour-reviews__item margin-left margin-right padding-top padding-bottom" >
+    	<div class="tour-reviews__item__container">
+	    	<div class="tour-reviews__item__info">
 		<?php echo get_avatar( $comment, apply_filters( 'woocommerce_review_gravatar_size', '60' ), '' ); ?>
 
 		<div class="comment-text">
+  <div class="tour-reviews__item__name" itemprop="author" itemscope itemtype="http://schema.org/Person"><span itemprop="name"><?php comment_author(); ?></span></div>
 
-			<?php if ( $rating && get_option( 'woocommerce_enable_review_rating' ) === 'yes' ) : ?>
-
-				<div itemprop="reviewRating" itemscope itemtype="http://schema.org/Rating" class="star-rating" title="<?php echo sprintf( __( 'Rated %d out of 5', 'woocommerce' ), $rating ) ?>">
-					<span style="width:<?php echo ( $rating / 5 ) * 100; ?>%"><strong itemprop="ratingValue"><?php echo $rating; ?></strong> <?php _e( 'out of 5', 'woocommerce' ); ?></span>
-				</div>
-
-			<?php endif; ?>
-
-			<?php do_action( 'woocommerce_review_before_comment_meta', $comment ); ?>
-
-			<?php if ( $comment->comment_approved == '0' ) : ?>
-
-				<p class="meta"><em><?php _e( 'Your comment is awaiting approval', 'woocommerce' ); ?></em></p>
-
-			<?php else : ?>
-
-				<p class="meta">
-					<strong itemprop="author"><?php comment_author(); ?></strong> <?php
-
-						if ( get_option( 'woocommerce_review_rating_verification_label' ) === 'yes' )
-							if ( $verified )
-								echo '<em class="verified">(' . __( 'verified owner', 'woocommerce' ) . ')</em> ';
-
-					?>&ndash; <time itemprop="datePublished" datetime="<?php echo get_comment_date( 'c' ); ?>"><?php echo get_comment_date( wc_date_format() ); ?></time>:
-				</p>
-
-			<?php endif; ?>
-
+					<div class="tour-reviews__item__content">
+			<div class="tour-reviews__item__content__top">
+				<?php do_action( 'woocommerce_review_before_comment_meta', $comment ); ?>
+				<?php if ( $rating && get_option( 'woocommerce_enable_review_rating' ) == 'yes' ) : ?>
+					<?php adventure_tours_renders_stars_rating( $rating, array(
+						'before' => '<div class="tour-reviews__item__rating">',
+						'after' => '</div>',
+					) ); ?>
+					<span itemprop="reviewRating" itemscope itemtype="http://schema.org/Rating">
+						<meta itemprop="ratingValue" content="<?php echo esc_html( $rating ); ?>">
+					</span>
+				<?php endif; ?>
+				<div class="tour-reviews__item__date"><?php echo get_comment_date( wc_date_format() ); ?></div>
+			</div>
 			<?php do_action( 'woocommerce_review_before_comment_text', $comment ); ?>
-
-			<div itemprop="description" class="description"><?php comment_text(); ?></div>
-
+			<div class="tour-reviews__item__text" itemprop="reviewBody"><?php comment_text(); ?></div>
 			<?php do_action( 'woocommerce_review_after_comment_text', $comment ); ?>
-
-				<div class="tour-reviews__item__container"> 
-		<div class="tour-reviews__item__info">
-
 		</div>
 	</div>
+	
+		
+	</div>
+	</div>	
+	
+	
+</div>	
+
+
 	
